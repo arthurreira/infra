f8c97d43e212c7ef5d1cbd773c413c2bd94f5b24
Arthur Ferreira <arthur.ferreiramiran@gmail.com>
2025-12-26 01:16:30 +0200

    Enable auto-deploy on app/ push

--- infra/main.tf
+++ infra/main.tf
@@ -57,6 +57,10 @@ resource "github_repository_file" "workflow" {
   content = <<-YAML
     name: Deploy static content to Pages
     on:
+      push:
+        branches: ["main"]
+        paths:
+          - 'app/**'
       workflow_dispatch:
     permissions:
       contents: read
